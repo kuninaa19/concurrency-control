b37d8cc7d48c291c466ad3f7c687f4960ed5bd22
kuninaa19 <minchan0527@naver.com>
2023-06-28 19:35:12 +0900

    RedissonConfig 설정

---
+++ src/main/java/com/event/fcfssystem/config/RedissonConfig.java
@@ -0,0 +1,23 @@
+package com.event.fcfssystem.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${redis.address}")
+    private String redisAddress;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisAddress);
+
+        return Redisson.create(config);
+    }
+}
